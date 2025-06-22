7918ad6a214ab282708c1d11142417a67a7ff828
Setche Atchade <setche.atchade@gmail.com>
2025-06-22 19:12:49 +0200

    Correction 00

--- script-numerique.js
+++ script-numerique.js
@@ -10,7 +10,6 @@ function updateClock() {
   hours = hours < 10 ? "0" + hours : hours;
   minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  milliseconds = milliseconds < 10 ? "0" : milliseconds;
 
   const timeString = `${hours}:${minutes}:${seconds}:${milliseconds}`; // Formate l'heure
   horlogeElement.textContent = timeString; //Met À jour le texte de l'élément HTML
